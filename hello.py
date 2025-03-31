54e52594a76b490ce4d669a27ae059d3d1b4c8b4
Aditya Anand <140170441+aditya19843@users.noreply.github.com>
2025-03-31 13:47:42 +0530

    new file:   Datatype.py 	new file:   hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+# This is a Single-line comment.
+'''
+This is a multi-line comment.'''
+"""
+# This is a multi-line comment."""
